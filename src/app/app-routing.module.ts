f8f37f738a3df0f664f2e1978a841008d4820814
rajneesh-nielsen <rajneesh.mishra@nielsen.com>
2018-02-21 14:23:30 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { CategoriesComponent } from './categories.component';
+import { CreateBlogComponent } from './create-blog.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'categories', component: CategoriesComponent }
+    { path: 'categories', component: CategoriesComponent },
+    { path: 'create-blog', component: CreateBlogComponent }
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { HomeComponent } from './home.component';
 import { CategoriesComponent } from './categories.component';
+import { CreateBlogComponent } from './create-blog.component';
 import { FooterComponent } from './footer.component';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { ArticlesService } from './services/articles.service';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, FooterComponent, HomeComponent, CategoriesComponent
+    AppComponent, HeaderComponent, FooterComponent, HomeComponent, CategoriesComponent, CreateBlogComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule, FormsModule, HttpClientModule
+    BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, FroalaEditorModule.forRoot(), FroalaViewModule.forRoot()
   ],
   providers: [ ArticlesService ],
   bootstrap: [AppComponent]
